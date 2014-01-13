ded9a4c45df1260404fbfdf57523b53d5cbcd3d8
Tom Ashworth <tashworth@twitter.com>
2014-01-13 17:44:14 +0000

    Rewrite with other tools in mind. Now a module.

--- index.js
+++ index.js
@@ -1,107 +1,24 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
-var chalk = require('chalk');
-var argv = require('optimist').argv;
 var kmeans = require('node-kmeans');
+var colorString = require('color-string');
 
-var file = fs.readFileSync(argv._[0], { encoding: 'utf8' });
+var regex = /(#[0-9a-f]{3,6})/gi;
 
-var regex = /\$([a-z0-9_]+)\s*:\s*#([0-9a-f]+)\s*;/gi;
+exports.clusterColors = function (colors, opts, cb) {
+    cb = cb || function () {};
 
-var colors = [];
-file.replace(regex, function (match, name, value) {
-    var result = extractHex(name, value);
-    if (result) {
-        colors.push(result);
-    }
-    return match;
-});
+    kmeans.clusterize(colors, { k: parseInt(opts.means || (colors.length * 0.8), 10) }, cb);
+};
 
-if (typeof argv.find !== "undefined") {
-    argv.find = ''+argv.find;
-}
-
-var showNew = false;
-if (typeof argv.new !== "undefined") {
-    argv.new = ''+argv.new;
-    showNew = true;
-    var result = extractHex('_', ''+argv.new);
-    if (result) {
-        colors.push(result);
-    }
-}
-
-var vectors = colors.map(function (color) {
-    return [color.r, color.g, color.b];
-});
-
-kmeans.clusterize(vectors, { k: parseInt(argv.means || (vectors.length * 0.8), 10) }, function(err, clusters) {
-    if (err) throw err;
-    clusters.forEach(function (cluster) {
-        var names = cluster.clusterInd.map(function (index) {
-            return colors[index].name;
-        });
-        var values = cluster.clusterInd.map(function (index) {
-            return colors[index].value;
-        });
-        var showCluster = names.length > 1;
-        if (argv.find) {
-            showCluster = ~names.indexOf(argv.find) ||
-                          ~values.indexOf(argv.find);
-        }
-        if (argv.new) {
-            showCluster = ~names.indexOf('_');
-        }
-        if (showCluster) {
-            console.log();
-            if (showNew) {
-                console.log(chalk.green('#' + argv.new));
-            }
-            cluster.clusterInd.forEach(function (index) {
-                if (colors[index].name === '_') return;
-                var name = '$' + colors[index].name;
-                var value = '#' + colors[index].value;
-                if (name === '$' + argv.find) {
-                    name = chalk.green(name);
-                }
-                if (value === '#' + argv.find) {
-                    value = chalk.green(value);
-                }
-                console.log(name, value);
-            });
-        }
-  });
-});
-
-/**
- * Utils
- */
-
-function extractHex(name, value) {
-    if (value.length === 3) {
-        return extract3Hex(name, value);
-    } else if (value.length === 6) {
-        return extract6Hex(name, value);
-    }
-}
-
-function extract3Hex(name, value) {
-    return {
-        name: name,
-        value: value,
-        r: parseInt(value.slice(0,1) + value.slice(0,1), 16),
-        g: parseInt(value.slice(1,2) + value.slice(1,2), 16),
-        b: parseInt(value.slice(2,3) + value.slice(2,3), 16)
-    };
-}
-
-function extract6Hex(name, value) {
-    return {
-        name: name,
-        value: value,
-        r: parseInt(value.slice(0,2), 16),
-        g: parseInt(value.slice(2,4), 16),
-        b: parseInt(value.slice(4,6), 16)
-    };
-}
+exports.extractColors = function (cssString) {
+    var colors = [];
+    cssString.replace(regex, function (match, value) {
+        try {
+            var result = colorString.getRgb(value);
+            colors.push(result);
+        } catch (e) {}
+        return match;
+    });
+    return colors;
+};
